9024a369c7a31e799a42232ffd888ea6193ee636
Fredy Laverde <fredys.laverdem@utadeo.edu.co>
2024-02-07 19:04:27 -0500

    avance shell

--- utils/sort.py
+++ utils/sort.py
@@ -1,4 +1,8 @@
 def shell_sort(vector):
+    contador_sublista = len(vector) // 2
+    while contador_sublista > 0:
+        for i in range(contador_sublista):
+            
     return vector
 
 def quick_sort(vector):
