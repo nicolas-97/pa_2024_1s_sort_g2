df793767c134f15482c8a5c65a7aea726e7e06ee
Stiven1013 <fabians.suarezc@utadeo.edu.co>
2024-02-13 17:22:44 -0500

    Shell sort done

--- utils/sort.py
+++ utils/sort.py
@@ -1,7 +1,21 @@
+
 def shell_sort(vector):
+     n = len(vector)
+    intervalo = n // 2
+    
+    while intervalo > 0:
+        for i in range(intervalo, n):
+            temp = vector[i]
+            j = i
+            while j >= intervalo and vector[j - intervalo] > temp:
+                vector[j] = vector[j - intervalo]
+                j -= intervalo
+                vector[j] = temp
+            intervalo //= 2
     return vector
 
 def quick_sort(vector):
+
     return vector
 
 def merge_sort(vector):
