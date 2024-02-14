0200e2fb8037f3cabeb320cd0e3fa6a4f80f2a01
Stiven1013 <fabians.suarezc@utadeo.edu.co>
2024-02-14 16:32:10 -0500

    Update

---
+++ utils/sort.py
@@ -0,0 +1,57 @@
+def shell_sort(vector):
+    if len(vector) < 2:
+        return vector
+    size = len(vector)
+    cut = size // 2
+    while cut > 0:
+        for i in range(cut, size):
+            shell = vector[i]
+            j = i
+            while j >= cut and vector[j - cut] > shell:
+                vector[j] = vector[j - cut]
+                j -= cut
+                shell = vector[j]
+        cut  //= 2
+    return vector
+
+def quick_sort(vector):
+    if len(vector)<2:
+        return vector
+
+    pivot=vector[0]
+    first=[x for x in vector[1:] if x<=pivot]   
+    last=[x for x in vector[1:] if x>pivot] 
+    vector=quick_sort(first) + [pivot] + quick_sort(last)
+    return vector
+
+
+def merge_sort(vector):
+
+    if len(vector)<2:
+        return vector
+    if len(vector) > 1:
+        middle = len(vector) // 2
+        left = vector[:middle]
+        right = vector[middle:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                vector[k] = left[i]
+                i += 1
+            else:
+                vector[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            vector[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            vector[k] = right[j]
+            j += 1
+            k += 1
+    return vector
